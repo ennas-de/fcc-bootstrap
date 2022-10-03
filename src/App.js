1a61f359e320c7e01db5d77d38ed6f1296119a28
Abdulhakeem <70843998+ennas-de@users.noreply.github.com>
2022-10-03 04:51:12 +0100

    FCC bootstrap tut

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <div className="row">
+        {/* <!-- Only change code above this line --> */}
+
+        <div class="container-fluid">
+          <h3 class="text-primary text-center">jQuery Playground</h3>
+          <div class="row">
+            <div class="col-xs-6">
+              <h4>#left-well</h4>
+              <div class="well" id="left-well">
+                <button class="btn btn-default target" id="target1">
+                  #target1
+                </button>
+                <button class="btn btn-default target" id="target2">
+                  #target2
+                </button>
+                <button class="btn btn-default target" id="target3">
+                  #target3
+                </button>
+              </div>
+            </div>
+            <div class="col-xs-6">
+              <h4>#right-well</h4>
+              <div class="well" id="right-well">
+                <button class="btn btn-default target" id="target4">
+                  #target4
+                </button>
+                <button class="btn btn-default target" id="target5">
+                  #target5
+                </button>
+                <button class="btn btn-default target" id="target6">
+                  #target6
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
